3535f6841dd6f96654cb2141fcd815f31eaf5cdb
reubn <reuben@t3ch.tk>
2014-03-04 08:14:52 +0000

    Expanded

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-$.get("//itunes.apple.com/gb/rss/topsongs/limit=50/explicit=true/xml").done(function(a) {
-  a = a.querySelectorAll("entry");
-  for (var b = 0;b < a.length;b++) {
-    var c = a[b].querySelectorAll("name")[0].textContent.split("(").join("").split(")").join(""), d = a[b].querySelectorAll("image")[2].textContent, e = a[b].querySelectorAll("artist")[0].textContent, f = encodeURIComponent(c);
-    $(".music").append('<div class="song" onclick="clickedSONG(\'' + addslashes(d) + "','" + addslashes(f) + '\')"><img class="art" src="' + addslashes(d) + '"><p>' + c + "</p><p>" + addslashes(e) + "</p></div>");
-  }
-});
+$.get("//itunes.apple.com/gb/rss/topsongs/limit=50/explicit=true/xml")
+    .done(function (data) {
+        var items = data.querySelectorAll("entry");
+
+        for (var n = 0; n < items.length; n++) {
+            var title = items[n].querySelectorAll("name")[0].textContent.split(/ *\([^)]*\) */g).join("");
+            var thumbs = items[n].querySelectorAll("image");
+            var thumbnail = thumbs[2].textContent;
+            var artistpre = items[n].querySelectorAll('artist');
+            var artist = artistpre[0].textContent;
+            var spotifyURL = encodeURIComponent(title);
+            $('.music').append('<div class="song" onclick="clickedSONG(\'' + addslashes(thumbnail) + '\',\'' + addslashes(spotifyURL) + '\')"><img class="art" src="' + addslashes(thumbnail) + '"><p>' + title + '</p><p>' + artist + '</p></div>');
+
+        }
+    });
+
 function addslashes(a) {
-  return(a + "").replace(/[\\"']/g, "\\$&").replace(/\u0000/g, "\\0");
+    return (a + "").replace(/[\\"']/g, "\\$&").replace(/\u0000/g, "\\0");
 }
-function clickedSONG(a, b) {
-  $.get("//ws.spotify.com/search/1/track", {q:b}).done(function(a) {
-    a = a.querySelectorAll("track")[0].getAttribute("href");
-    location.href = a;
-  });
-  location.href = "spotify:search:" + b;
-  $(".chngBG").remove();
-  $("head").append("<style class='chngBG'>body::before{ background-image:url(" + a + ")!important;}</style>");
+
+function clickedSONG(bgURL, spotifyURL) {
+    $.get("//ws.spotify.com/search/1/track", {
+        "q": spotifyURL
+    })
+        .done(function (data) {
+            var item = data.querySelectorAll("track")[0].getAttribute('href');
+            location.href = item;
+
+        });
+    //location.href = spotifyURL;
+    $(".chngBG").remove();
+    $('head').append("<style class='chngBG'>body\:\:before{ background-image\:url(" + bgURL + ")!important;}</style>");
 }
