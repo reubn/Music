175c16211e6e68de13ef4f9b6e857e3c37415436
reubn <reuben@t3ch.tk>
2014-03-08 14:02:03 +0000

    ditto

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ if (!isMobile.any()) {
             } else {
                 iTunesOnly = true;
             }
-            console.log('This ↑ ' + jqXHR.status + ' is checking if Spotify is installed ;\)');
+            console.log('This ↑ ' + jqXHR.status + ' ↑ is checking if Spotify is installed ;\)');
             console.info(jqXHR);
         }
 
