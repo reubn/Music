e77f65bc6924c5af1996f9f138038a3b8fb07c8e
reubn <reuben@t3ch.tk>
2014-03-09 12:03:09 +0000

    Uses arrays + not as confusing

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
+//Smooth Scroll Hack
 $('.music').on('touchstart', function(event){});
-var iTunesOnly;
+//Vars
+var music = [];
+var hasSpotify;
 var audio = $(".player");
-var isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-if (!isMobile.any()) {
+var isMobile = {a: function () {return navigator.userAgent.match(/Android/i);},b: function () {return navigator.userAgent.match(/BlackBerry/i);},i: function () {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},o: function () {return navigator.userAgent.match(/Opera Mini/i);},w: function () { return navigator.userAgent.match(/IEMobile/i);},t: function () { return (isMobile.a() || isMobile.b() || isMobile.i() || isMobile.o() || isMobile.w());}};
+
+if (!isMobile.t()) {
     $.ajax({
         type: 'GET',
         dataType: 'script',
@@ -30,16 +15,16 @@ if (!isMobile.any()) {
         error: function (jqXHR) {
 
             if (jqXHR.status == '404') {
-                iTunesOnly = false;
+                hasSpotify = true;
             } else {
-                iTunesOnly = true;
+                hasSpotify = false;
             }
-            console.log('This ↑ ' + jqXHR.status + ' ↑ is checking if Spotify is installed ;\)');
+            console.info('This ↑ ' + jqXHR.status + ' ↑ is checking if Spotify is installed ;\)');
         }
 
     });
 } else{
-iTunesOnly = true;
+hasSpotify = false;
 }
 if (window.location.hash) {
     hashPlay = window.open(window.location.hash.split("#").join("spotify:track:"));
@@ -49,19 +34,17 @@ $.get("http://itunes.apple.com/gb/rss/topsongs/limit=50/explicit=true/xml", func
     var items = data.getElementsByTagName("entry");
 
     for (var n = 0; n < items.length; n++) {
-        var title = items[n].querySelector("name").textContent.split(/ *\([^)]*\) */g).join("");
+        var title = items[n].querySelector("name").textContent.split(/ *\[[^)]*\] *| *\([^)]*\) */g).join("");
+        var artist = items[n].querySelector('artist').textContent;
         var thumbnail = items[n].getElementsByTagName("image")[2].textContent.split("170x170").join("340x340");
         var itunesURL = items[n].getElementsByTagName('link')[1].getAttribute('href');
-        var artist = items[n].querySelector('artist').textContent;
-        var spotifyURL = encodeURIComponent(title);
-        $('.music').append('<div class="song" onclick="clickedSONG(\'' + addslashes(thumbnail) + '\',\'' + addslashes(spotifyURL) + '\',\'' + addslashes(itunesURL) + '\',\'' + artist + '\')"><img class="art" src="' + addslashes(thumbnail) + '"><p>' + title + '</p><p>' + artist + '</p></div>');
+        var songInfo = [title, artist, thumbnail, itunesURL, encodeURIComponent(title)];
+        music.push(songInfo);
+        var songPosition = music.length-1;
+        $('.music').append('<div class="song" onclick="clickedSONG(' + songPosition + ')"><img class="art" src="' + thumbnail + '"><p>' + title + '</p><p>' + artist + '</p></div>');
 
     }
 });
-
-function addslashes(a) {
-    return (a + "").replace(/[\\"']/g, "\\$&").replace(/\u0000/g, "\\0");
-}
 
 function playiTunes(itunesURL) {
     $("#playersrc").attr("src", itunesURL);
@@ -70,32 +53,33 @@ function playiTunes(itunesURL) {
     audio[0].play();
 }
 
-function clickedSONG(bgURL, spotifyURL, itunesURL, artist) {
-    var art = artist;
+function clickedSONG(songPosition) {
+    var song = music[songPosition];
+    var art = song[1];
     //console.log(art.trim().toLowerCase().split(" ")[0]);
     audio[0].pause();
-    if (iTunesOnly === false) {
+    if (hasSpotify === true) {
         $.get("//ws.spotify.com/search/1/track", {
-            "q": spotifyURL
+            "q": song[4]
         }, function (data) {
             var num = 0;
             var found = false;
             while (found === false) {
                 //console.log(data.querySelectorAll("artist")[num].textContent.trim().toLowerCase().split(" ")[0]);
                 if (data.querySelectorAll("artist")[num].textContent.trim().toLowerCase().split(" ")[0] == art.trim().toLowerCase().split(" ")[0]) {
-                    //console.log('True' + num);
+                    //console.log('True ' + num);
                     var url = data.querySelectorAll("track")[num].getAttribute('href');
+                    music[songPosition].push(url);
                     history.pushState(url, "", url.split("spotify:track:").join("#"));
-                    //window.location.assign("spotify:search:track:" + spotifyURL);
                     location.href = url;
                     found = true;
                 } else {
                     num++;
-                    //console.log('False' + num);
+                    //console.log('False ' + num);
                     found = false;
                 };
                 if (num == 20) {
-                    playiTunes(itunesURL);
+                    playiTunes(song[3]);
                     break;
                 }
             }
@@ -103,8 +87,8 @@ function clickedSONG(bgURL, spotifyURL, itunesURL, artist) {
 
         });
     } else {
-        playiTunes(itunesURL);
+        playiTunes(song[3]);
     }
     $(".chngBG").remove();
-    $('head').append("<style class='chngBG'>body::before{ background-image:url(" + bgURL + ")!important;}</style>");
+    $('head').append("<style class='chngBG'>body::before{ background-image:url(" + song[2] + ")!important;}</style>");
 }
