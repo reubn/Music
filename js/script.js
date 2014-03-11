a257311b04b1a6f074ae7ab8b9cde4ab1152634f
reubn <reuben@t3ch.tk>
2014-03-11 17:58:33 +0000

    Uses JSON

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ if (window.location.hash) {
         dataType: 'jsonp',
         url: '//embed.spotify.com/oembed/',
         data: {
-            url: window.location.hash.split("#").join("spotify:track:")
+            url: window.location.hash.replace(/#/g, "spotify:track:")
         }
     }).done(function (data) {
-        $('head').append("<style class='chngBG'>body::before{ background-image:url(" + data.thumbnail_url.split("cover").join("640") + ")!important;}</style>");
+        $('head').append("<style class='chngBG'>body::before{ background-image:url(" + data.thumbnail_url.replace(/cover/g, "640") + ")!important;}</style>");
     });
-    hashPlay = window.open(window.location.hash.split("#").join("spotify:track:"));
+    hashPlay = window.open(window.location.hash.replace(/#/g, "spotify:track:"));
     hashPlay.close();
 }
 
@@ -62,65 +62,68 @@ if (!isMobile.t()) {
 } else {
     hasSpotify = false;
 }
-$.get("http://itunes.apple.com/gb/rss/topsongs/limit=50/explicit=true/xml", function (data) {
-    var items = data.getElementsByTagName("entry");
-    
-    for (var n = 0; n < items.length; n++) {
-        var title = items[n].querySelector("name").textContent.split(/ *\[[^)]*\] *| *\([^)]*\) */g).join("");
-        var artist = items[n].querySelector('artist').textContent;
-        var thumbnail = items[n].getElementsByTagName("image")[2].textContent.split("170x170").join("340x340");
-        var itunesURL = items[n].getElementsByTagName('link')[1].getAttribute('href');
-        var songInfo = [title, artist, thumbnail, itunesURL, encodeURIComponent(title)];
-        music.push(songInfo);
-        var songPosition = music.length - 1;
-        $('.music').append('<div class="song" onclick="clickedSONG(' + songPosition + ')"><img class="art" src="' + thumbnail + '"><p>' + title + '</p><p>' + artist + '</p></div>');
+$.ajax({
+        type: 'GET',
+        url: "http://itunes.apple.com/gb/rss/topsongs/limit=50/explicit=true/json",
+        dataType: 'json',
+        success: function (data) {
+            var items = data.feed.entry;
 
-    }
-});
+            for (var n = 0; n < items.length; n++) {
+                var title = items[n]['im:name'].label.replace(/ *\[[^)]*\] *| *\([^)]*\) */g, "");
+                var artist = items[n]['im:artist'].label;
+                var thumbnail = items[n]['im:image'][2].label.replace(/170x170/g, "340x340");
+                var itunesURL = items[n].link[1].attributes.href;
+                var songInfo = [title, artist, thumbnail, itunesURL, encodeURIComponent(title)];
+                music.push(songInfo);
+                $('.music').append('<div class="song" onclick="clickedSONG(' + (music.length-1) + ')"><img class="art" src="' + thumbnail + '"><p>' + title + '</p><p>' + artist + '</p></div>');
 
-function playiTunes(itunesURL) {
-    $("#playersrc").attr("src", itunesURL);
-    audio[0].pause();
-    audio[0].load();
-    audio[0].play();
-}
-
-function clickedSONG(songPosition) {
-    var song = music[songPosition];
-    var artist = song[1];
-    //console.log(art.trim().toLowerCase().split(" ")[0]);
-    audio[0].pause();
-    if (hasSpotify === true) {
-        $.get("//ws.spotify.com/search/1/track", {
-            "q": song[4]
-        }, function (data) {
-            var num = 0;
-            var found = false;
-            while (found === false) {
-                //console.log(data.querySelectorAll("artist")[num].textContent.trim().toLowerCase().split(" ")[0]);
-                if (data.querySelectorAll("artist")[num].textContent.trim().toLowerCase().split(" ")[0] == artist.trim().toLowerCase().split(" ")[0]) {
-                    //console.log('True ' + num);
-                    var url = data.querySelectorAll("track")[num].getAttribute('href');
-                    music[songPosition].push(url);
-                    history.pushState(url, "", url.split("spotify:track:").join("#"));
-                    location.href = url;
-                    found = true;
-                } else {
-                    num++;
-                    //console.log('False ' + num);
-                    found = false;
-                };
-                if (num == 20) {
-                    playiTunes(song[3]);
-                    break;
-                }
             }
-
-
+        }
         });
-    } else {
-        playiTunes(song[3]);
+
+    function playiTunes(itunesURL) {
+        $("#playersrc").attr("src", itunesURL);
+        audio[0].pause();
+        audio[0].load();
+        audio[0].play();
     }
-    $(".chngBG").remove();
-    $('head').append("<style class='chngBG'>body::before{ background-image:url(" + song[2] + ")!important;}</style>");
-}
+
+    function clickedSONG(songPosition) {
+        var song = music[songPosition];
+        var artist = song[1];
+        //console.log(art.trim().toLowerCase().replace(/ ")[0]);
+        audio[0].pause();
+        if (hasSpotify === true) {
+            $.get("//ws.spotify.com/search/1/track.json", {
+                "q": song[4]
+            }, function (data) {
+                var num = 0;
+                var found = false;
+                while (found === false) {
+                    if (data.tracks[num].artists[0].name.trim().toLowerCase().split(" ")[0] == artist.trim().toLowerCase().split(" ")[0]) {
+                        //console.log('True ' + num);
+                        var url = data.tracks[num].href;
+                        music[songPosition].push(url);
+                        history.pushState(url, "", url.replace(/spotify:track:/g, "#"));
+                        location.href = url;
+                        found = true;
+                    } else {
+                        num++;
+                        //console.log('False ' + num);
+                        found = false;
+                    };
+                    if (num == 20) {
+                        playiTunes(song[3]);
+                        break;
+                    }
+                }
+
+
+            });
+        } else {
+            playiTunes(song[3]);
+        }
+        $(".chngBG").remove();
+        $('head').append("<style class='chngBG'>body::before{ background-image:url(" + song[2] + ")!important;}</style>");
+    }
